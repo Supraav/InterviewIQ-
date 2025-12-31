8324b391fafeaa20a2daeb7bf6b2303a2c7bf71a
Supraav <supravadhikari@example.com>
2026-01-01 00:02:08 +0545

    testing connection

---
+++ readme.md
@@ -0,0 +1,33 @@
+# AI Employee Interview Helper
+An AI-powered logic engine designed to help students/employees by generating interview questions and analyzing responses. This project uses the **OpenRouter API** to access a variety of LLMs (like GPT-4, Qwen, or Llama) through a single interface.
+
+## 🛠️ Setup & Installation
+
+### 1. Configure the Environment
+Ensure you have a `.env` file in the root directory with the following variables:
+```text
+OPENROUTER_API_KEY=your_openrouter_key_here 
+```
+### 2. Setup Virtual Environment
+
+```# Create the environment
+python -m venv env
+
+# Activate it (Windows)
+env\Scripts\activate
+
+# Activate it (Mac/Linux)
+source ./env/bin/activate
+```
+
+### 3. Install Dependencies
+
+```
+pip install -r requirements.txt 
+```
+
+### 4. Run 
+
+```
+python3 test_brain.py
+```
---
+++ test_brain.py
@@ -0,0 +1,38 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+
+def get_client():
+    api_key = os.getenv("OPENROUTER_API_KEY")
+
+    if not api_key:
+        raise ValueError("OPENROUTER_API_KEY not found in env")
+
+    client = OpenAI(base_url="https://openrouter.ai/api/v1", api_key=api_key)
+
+    return client
+
+
+def test_connection():
+    try:
+        client = get_client()
+
+        completion = client.chat.completions.create(
+            model="qwen/qwen3-coder:free",
+            messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": "i am sad "}
+                ],
+        )
+        print("connected to openrouter API")
+        print("Model Response:", completion.choices[0].message.content)
+        # import pdb;pdb.set_trace()
+    
+    except Exception as e:
+        print("Failed to connect to openrouter API", str(e))
+
+if __name__ == "__main__":
+    test_connection()

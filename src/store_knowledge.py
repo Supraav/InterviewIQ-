7bc45289d2ec40df94cce84cc025dcd3b5dc292f
Supraav <supravadhikari@example.com>
2026-01-02 00:33:34 +0545

    folder restructuring

---
+++ src/store_knowledge.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+load_dotenv()
